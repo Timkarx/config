c9f0818f620ac6ccb37bf0bb34c788e55be6456b
Tim <ttsutskarev@gmail.com>
2025-12-01 17:45:08 +0200

    fix markdown preview (cd app && yarn install)

--- nvim/lua/plugins/md_preview.lua
+++ nvim/lua/plugins/md_preview.lua
@@ -1,6 +1,15 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
   ft = { "markdown" },
-  build = function() vim.fn["mkdp#util#install"]() end,
+  config = function()
+    vim.g.mkdp_auto_close = true
+    vim.g.mkdp_open_to_the_world = false
+    vim.g.mkdp_open_ip = "127.0.0.1"
+    vim.g.mkdp_port = "8888"
+    vim.g.mkdp_browser = ""
+    vim.g.mkdp_echo_preview_url = true
+    vim.g.mkdp_page_title = "${name}"
+  end,
 }
