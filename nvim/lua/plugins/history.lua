fa6eb0d127bd4e644f30e7fa746f1792487c3b2e
Tim <ttsutskarev@gmail.com>
2026-03-28 22:19:39 +0300

    custom history mappings: backspace -> back, enter -> forward

--- nvim/lua/plugins/history.lua
+++ nvim/lua/plugins/history.lua
@@ -1,6 +1,11 @@
 return {
     dir = '~/projects/zap',
     config = function ()
-        require('history').setup()
+        require('history').setup({
+            keybinds = {
+                back = '<BS>',
+                forward = '<CR>',
+            }
+        })
     end
 }
