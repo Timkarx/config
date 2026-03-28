7c6686f38c110fe2480524fe62651fe18f9238ae
Tim <ttsutskarev@gmail.com>
2026-03-28 18:46:56 +0300

    update telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim', version = '*',
 -- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' },
+      dependencies = { 'nvim-lua/plenary.nvim', { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }, },
 	config = function () 
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files (respect .gitignore' })
